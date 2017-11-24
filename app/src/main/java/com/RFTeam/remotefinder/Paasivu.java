b9fa9a26430ed5a6002aeeed7505209ca4c4fb64
Tomi Mäkinen <tomi.makinen@hotmail.fi>
2017-11-24 14:27:55 +0200

    Projekti luotu

---
+++ app/src/main/java/com/RFTeam/remotefinder/Paasivu.java
@@ -0,0 +1,13 @@
+package com.RFTeam.remotefinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Paasivu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paasivu);
+    }
+}
