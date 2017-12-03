f92e942e5ec73d2e3e3eccaa9f483e0a59638a1e
Tomi Mäkinen <tomi.makinen@hotmail.fi>
2017-12-03 17:19:12 +0200

    Added state.java. this contains static int values corresponding to different states of the program

---
+++ app/src/main/java/com/RFTeam/remotefinder/STATE.java
@@ -0,0 +1,18 @@
+package com.RFTeam.remotefinder;
+
+/**
+ * Created by rytai on 3.12.2017.
+ */
+
+public final class STATE {
+
+    public static int NO_BLUETOOTH = 0;
+    public static int BLUETOOTH_ENABLED = 1;
+    public static int BLUETOOTH_DISCOVERING = 2;
+    public static int GATT_CONNECTING = 3;
+    public static int GATT_CONNECTED = 4;
+    public static int READING_RSSI = 5;
+    public static int CURRENT_DEVICE_DISCONNECTED = 6;
+    public static int DEVICE_SELECTED = 7;
+    public static int UNDEFINED = 100;
+}
